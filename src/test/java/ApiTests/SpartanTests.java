27ebbba75d116c1b1ff1a63983c3de566c21710d
zübeyir <zeynelzatari@gmail.com>
2023-02-04 21:42:43 +0100

    Repeat

--- src/test/java/ApiTests/SpartanTests.java
+++ src/test/java/ApiTests/SpartanTests.java
@@ -1,7 +1,9 @@
 package ApiTests;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SpartanTests {
@@ -25,5 +27,44 @@ public class SpartanTests {
 
     }
 
+    /*
+    When user send Get request to /api/spartans end point
+    Then status code must be 200
+    And body should contain Allen --> bu Allen herhangi bir isim. yani sen baska ismi de test edebilirsin varmi yok mu diye
+     */
+    @Test
+    public void viewSpartanTest2() {
+
+        Response response = RestAssured.get(spartanBaseUrl + "/api/spartans");
+
+        // verify status code 200
+        Assert.assertEquals(response.statusCode(), 200);
+
+        // verify body contain Allen
+        Assert.assertTrue(response.body().asString().contains("Allen"));
+
+    }
+
+    /*
+    Given Accept type is Json
+    When user sends Get request to spartanAllUrl
+    Then status code must be 200
+    Then response body should be Json format
+     */
+    @Test
+    public void viewSpartanTest3() {
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get(spartanBaseUrl + "/api/spartans");
+
+        // verify status code 200
+        Assert.assertEquals(response.statusCode(), 200);
+
+        // verify response body Json format
+        Assert.assertEquals(response.contentType(),"application/json");
+
+    }
 
 }
+
+// 9. video da kaldik
